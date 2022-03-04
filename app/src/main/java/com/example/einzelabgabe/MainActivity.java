85cf470fca6bfe3fe618695f6894012d37b7e48d
Dominik Pototschnig <dpotot@gmail.com>
2022-03-04 23:39:27 +0100

    Updated Layout according to the instructions on the sheet. 2 Buttons. Same TestArea for Calculation and Server Response

--- app/src/main/java/com/example/einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.einzelabgabe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,33 +12,41 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Button btAbschicken;
+    private Button btBerechne4;
     private EditText txtEditMatrikelnummer;
     private TextView answer;
-    private TextView output;
+    private TextView heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btAbschicken = findViewById(R.id.send);
+        btAbschicken = findViewById(R.id.button_send);
+        btBerechne4 = findViewById(R.id.button_calculate);
         txtEditMatrikelnummer = findViewById(R.id.editMatrikelnummer);
         answer = findViewById(R.id.answerServer);
-        output = findViewById(R.id.output);
+        heading = findViewById(R.id.heading2);
 
         btAbschicken.setOnClickListener(view -> {
+            heading.setText(getString(R.string.heading2));
+            answer.setText("");
             //Meine Matrikelnummer: 51813675
             String matrikelnummer = txtEditMatrikelnummer.getText().toString();
             ThreadServer askServer = new ThreadServer(matrikelnummer, answer);
             new Thread(askServer).start();
+            //txtEditMatrikelnummer.setText("");
+        });
 
-            txtEditMatrikelnummer.setText("");
-
+        btBerechne4.setOnClickListener(view -> {
+            heading.setText(getString(R.string.heading3));
+            answer.setText("");
+            String matrikelnummer = txtEditMatrikelnummer.getText().toString();
             int quersumme = 0;
             for(char c : matrikelnummer.toCharArray()) {
                 quersumme += Integer.parseInt(String.valueOf(c));
             }
-            output.setText(Integer.toBinaryString(quersumme));
+            answer.setText(Integer.toBinaryString(quersumme));
             //Log.i("Quersumme",Integer.toString(quersumme));
         });
     }
